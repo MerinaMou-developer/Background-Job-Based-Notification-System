1fc6af907fece40af32cb2ad32069fcb00be0231
Merina Mou <merinamou7@gmail.com>
2026-05-23 14:57:13 +0600

    feat(users): add email-based custom user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        db_table = "users"
+        ordering = ["-date_joined"]
+
+    def __str__(self):
+        return self.email
